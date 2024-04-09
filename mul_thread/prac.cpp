1aa490bde53367c8df223ead6b76243ccd84e554
2002OID <ykkim308321@gmail.com>
2024-04-09 14:38:09 +0900

    remove_atomic

--- mul_thread/prac.cpp
+++ mul_thread/prac.cpp
@@ -3,69 +3,35 @@
 #include <chrono>
 #include <vector>
 #include <mutex>
-#include <atomic>
-const auto MAX_THREADS = 64;
-std::atomic <int> a_sum;
+const auto max_threads = 64;
+volatile int sum;
 std::mutex mylock;
 
 
-void thread_func(const int num_threads) {
+void thread_func(int num_threads) {
+	volatile int local_sum = 0;
 	for (auto i = 0; i < 50000000 / num_threads; ++i) {
-		a_sum += 2;
+		
+		local_sum += 2;
+		
 	}
-
+	mylock.lock();
+	sum += local_sum;
+	mylock.unlock();
+	
 }
 int main() {
 	std::vector<std::thread> threads;
-	for (auto i = 1; i <= MAX_THREADS; i *= 2) {
-		a_sum = 0;
+	for (auto i = 1; i <= max_threads; i *= 2) {
+		sum = 0;
 		threads.clear();
 		auto start = std::chrono::high_resolution_clock::now();
 		for (auto j = 0; j < i; ++j) threads.push_back(std::thread{ thread_func, i });
 
 		for (auto& tmp : threads) tmp.join();
-
+		
 		auto duration = std::chrono::high_resolution_clock::now() - start;
-		std::cout << i << " Threads, Sum = " << a_sum;
-		std::cout << " Duration = " << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() << " milliseconds\n";
+		std::cout << i << " threads, sum = " << sum;
+		std::cout << " duration = " << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() << " milliseconds\n";
 	}
 }
-
-
-//#include <iostream>
-//#include <thread>
-//#include <chrono>
-//#include <vector>
-//#include <mutex>
-//const auto MAX_THREADS = 64;
-//volatile int sum;
-//std::mutex mylock;
-//
-//
-//void thread_func(int num_threads) {
-//	volatile int local_sum = 0;
-//	for (auto i = 0; i < 50000000 / num_threads; ++i) {
-//		
-//		local_sum += 2;
-//		
-//	}
-//	mylock.lock();
-//	sum += local_sum;
-//	mylock.unlock();
-//	
-//}
-//int main() {
-//	std::vector<std::thread> threads;
-//	for (auto i = 1; i <= MAX_THREADS; i *= 2) {
-//		sum = 0;
-//		threads.clear();
-//		auto start = std::chrono::high_resolution_clock::now();
-//		for (auto j = 0; j < i; ++j) threads.push_back(std::thread{ thread_func, i });
-//
-//		for (auto& tmp : threads) tmp.join();
-//		
-//		auto duration = std::chrono::high_resolution_clock::now() - start;
-//		std::cout << i << " Threads, Sum = " << sum;
-//		std::cout << " Duration = " << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() << " milliseconds\n";
-//	}
-//}
